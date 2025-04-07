f4d8afc8ce5e87afe7fd8dda107ee5803e8033d4
Elizabeth Paige Harper <elizabeth.harper@foxcapades.io>
2025-04-07 15:15:16 -0400

    add statement shortcuts for connection

--- src/main/kotlin/io/foxcapades/kdbc/stmt-rs.kt
+++ src/main/kotlin/io/foxcapades/kdbc/stmt-rs.kt
@@ -1,5 +1,6 @@
 package io.foxcapades.kdbc
 
+import java.sql.Connection
 import java.sql.ResultSet
 import java.sql.Statement
 import kotlin.contracts.ExperimentalContracts
@@ -70,4 +71,70 @@ inline fun <T> Statement.withStatementResults(sql: String, action: ResultSet.() 
 inline fun <T> Statement.usingStatementResults(sql: String, action: (ResultSet) -> T): T {
   contract { callsInPlace(action, InvocationKind.EXACTLY_ONCE) }
   return executeQuery(sql).use(action)
+}
+
+/**
+ * Creates a new [Statement] using [Connection.createStatement], then executes
+ * the new `Statement` using [Statement.executeQuery], calling the given
+ * [action] with the returned [ResultSet] as its receiver value, passing up
+ * the value returned by execution of the given action function and closing the
+ * `ResultSet` on return.
+ *
+ * Example:
+ * ```kotlin
+ * val record = con.usingStatementResults(sql) {
+ *   if (next())
+ *     MyRecord(getString(1), getInt(2), getBoolean(3))
+ *   else
+ *     null
+ * }
+ * ```
+ *
+ * @receiver `Connection` instance from which a new `Statement` will be created.
+ *
+ * @param action Function that will be called with the new `ResultSet` instance
+ * as its receiver.
+ *
+ * @return The value that was returned from executing the given [action].
+ *
+ * @see usingStatementResults
+ * @see withResults
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun <T> Connection.withStatementResults(sql: String, action: ResultSet.() -> T): T {
+  contract { callsInPlace(action, InvocationKind.EXACTLY_ONCE) }
+  return createStatement().executeQuery(sql).use(action)
+}
+
+/**
+ * Creates a new [Statement] using [Connection.createStatement], then executes
+ * the new `Statement` using [Statement.executeQuery], calling the given
+ * [action] with the returned [ResultSet] as its receiver value, passing up
+ * the value returned by execution of the given action function and closing the
+ * `ResultSet` on return.
+ *
+ * Example:
+ * ```kotlin
+ * val record = con.usingStatementResults(sql) {
+ *   if (next())
+ *     MyRecord(getString(1), getInt(2), getBoolean(3))
+ *   else
+ *     null
+ * }
+ * ```
+ *
+ * @receiver `Connection` instance from which a new `Statement` will be created.
+ *
+ * @param action Function that will be called with the new `ResultSet` instance
+ * as its input argument.
+ *
+ * @return The value that was returned from executing the given [action].
+ *
+ * @see usingStatementResults
+ * @see withResults
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun <T> Connection.usingStatementResults(sql: String, action: (ResultSet) -> T): T {
+  contract { callsInPlace(action, InvocationKind.EXACTLY_ONCE) }
+  return createStatement().executeQuery(sql).use(action)
 }
